24bf8165f4cf681284211d352e7ecb924ada2c29
yigitalidev <yigitaliarslan27@gmail.com>
2022-03-05 22:10:03 +0300

    Add files via upload

---
+++ CreditCardVerification/CreditCardVerification/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace CreditCardVerification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(126, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(389, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CreditCardVerification.Properties.Resources.kisspng_credit_card_debit_card_mastercard_payment_card_card_chip_5b48d11368a320_0380554215314987714286;
+            this.ClientSize = new System.Drawing.Size(825, 460);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CreditCardVerification/CreditCardVerification/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreditCardVerification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+           
+        int[] dizi = new int[16];
+        int toplam = 0;
+        int sayac = 0;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var i in textBox1.Text)
+                {
+                    int sayi = Convert.ToInt16(i.ToString());
+                    dizi[sayac] = sayi;
+                    if (sayac % 2 == 0)
+                    {
+                        dizi[sayac] *= 2;
+
+                        if (dizi[sayac] >= 10)
+                        {
+                            dizi[sayac] -= 10;
+                            dizi[sayac]++;
+                        }
+                        toplam += dizi[sayac];
+
+                    }
+                    else if (sayac % 2 == 1)
+                    {
+                        toplam += dizi[sayac];
+
+                    }
+                    sayac++;
+                }
+
+                if (textBox1.Text.Length == 16 && toplam % 10 == 0)
+                {
+                    label1.Text = "Kart Bilgileri Doğru";
+                    label1.ForeColor = Color.Green;
+                    toplam = 0;
+                    sayac = 0;
+                }
+                else
+                {
+                    label1.Text = "Kart Bilgileri Yanlış";
+                    label1.ForeColor = Color.Red;
+                    toplam = 0;
+                    sayac = 0;
+                }
+
+                if (textBox1.Text.Length == 0)
+                {
+                    label1.Text = "Lütfen Sayı Giriniz.";
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("16 karakteri aşamazsınız yada sayı dışında birşey giremezsiniz.", "Sistem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                toplam = 0;
+                sayac = 0;
+                textBox1.Clear();
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Lütfen sayı giriniz.";
+            label1.ForeColor = Color.Red;
+        }
+    }    
+}
